e0cb53ff727089e891768f72476516a6318a5db0
lgh <lgh95m@naver.com>
2021-12-03 11:34:11 +0900

    initial commit

---
+++ src/main/java/kr/omen/pico/PicoApplication.java
@@ -0,0 +1,13 @@
+package kr.omen.pico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/omen/pico/PicoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.omen.pico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
