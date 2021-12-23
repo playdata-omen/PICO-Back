dbd90e3232e778ce974440e04c672019e485dc5a
unknown <wlaud5314@naver.com>
2021-12-23 11:44:29 +0900

    [Update ChatRoomInfo]

---
+++ src/main/java/kr/omen/pico/controller/ChatRoomController.java
@@ -0,0 +1,23 @@
+package kr.omen.pico.controller;
+
+import kr.omen.pico.dao.ChatRoomRepository;
+import kr.omen.pico.domain.ChatRoom;
+import kr.omen.pico.domain.dto.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatRoomController {
+    private final ChatRoomRepository chatRoomRepository;
+
+    @GetMapping("/chatroom/{chatroomidx}")
+    public ResponseDTO.ChatRoomResponse findOneChatRoom(@PathVariable Long chatroomidx) {
+        ChatRoom chatRoom = chatRoomRepository.findById(chatroomidx).get();
+        return new ResponseDTO.ChatRoomResponse(chatRoom);
+    }
+}
