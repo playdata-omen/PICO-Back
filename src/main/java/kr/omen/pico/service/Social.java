eae069de06492b261d11aa99b8fb4a93f982a552
jh <sjj0077@naver.com>
2021-12-24 09:18:51 +0900

    travis test

--- src/main/java/kr/omen/pico/service/Social.java
+++ src/main/java/kr/omen/pico/service/Social.java
@@ -37,26 +37,27 @@ public class Social {
 
         HttpEntity<MultiValueMap<String, String>> kakaoRequest = new HttpEntity<>(data, headers);
         RestTemplate restTemplate = new RestTemplate();
+
         try {
-        response = restTemplate.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoRequest,
-                JSONObject.class
-        );
+            response = restTemplate.exchange(
+                    "https://kauth.kakao.com/oauth/token",
+                    HttpMethod.POST,
+                    kakaoRequest,
+                    JSONObject.class
+            );
 
-        JSONObject responseBody = response.getBody();
+            JSONObject responseBody = response.getBody();
 
-        headers.clear();
-        data.clear();
-        headers.add("Authorization", "Bearer "+responseBody.get("access_token"));
-        kakaoRequest = new HttpEntity<>(data, headers);
-        response = restTemplate.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                kakaoRequest,
-                JSONObject.class
-        );
+            headers.clear();
+            data.clear();
+            headers.add("Authorization", "Bearer "+responseBody.get("access_token"));
+            kakaoRequest = new HttpEntity<>(data, headers);
+            response = restTemplate.exchange(
+                    "https://kapi.kakao.com/v2/user/me",
+                    HttpMethod.POST,
+                    kakaoRequest,
+                    JSONObject.class
+            );
         } catch (HttpStatusCodeException e) {
             e.printStackTrace();
         }
@@ -83,6 +84,7 @@ public class Social {
         HttpEntity<MultiValueMap<String, String>> naverRequest =
                 new HttpEntity<>(data, headers);
         RestTemplate restTemplate = new RestTemplate();
+
         try {
             response = restTemplate.exchange(
                     "https://nid.naver.com/oauth2.0/token",
@@ -129,6 +131,7 @@ public class Social {
 
         HttpEntity<MultiValueMap<String, String>> googleRequest = new HttpEntity<>(data, headers);
         RestTemplate restTemplate = new RestTemplate();
+
         try {
             response = restTemplate.exchange(
                     "https://oauth2.googleapis.com/token",
