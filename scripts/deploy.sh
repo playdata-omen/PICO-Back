1924caf77fbed8b26283570cc04dda0c409dba52
jh <sjj0077@naver.com>
2021-12-26 15:12:52 +0900

    deploy test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,4 +20,4 @@ JAR_NAME=$(ls $REPOSITORY |grep 'pico' | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-nohup java -jar $REPOSITORY/jar/$JAR_NAME 2>&1 &
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
