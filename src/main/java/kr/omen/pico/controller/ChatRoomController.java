79fca68a814e9053770ccd19f60357ac5829a06a
jh <sjj0077@naver.com>
2021-12-26 12:18:20 +0900

    deploy test

--- src/main/java/kr/omen/pico/controller/ChatRoomController.java
+++ src/main/java/kr/omen/pico/controller/ChatRoomController.java
@@ -1,16 +1,18 @@
 package kr.omen.pico.controller;
 
-import kr.omen.pico.dao.ChatRoomRepository;
-import kr.omen.pico.domain.ChatRoom;
-import kr.omen.pico.domain.dto.ResponseDTO;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.omen.pico.dao.ChatRoomRepository;
+import kr.omen.pico.domain.ChatRoom;
+import kr.omen.pico.domain.dto.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequiredArgsConstructor
 public class ChatRoomController {
+
     private final ChatRoomRepository chatRoomRepository;
 
     @GetMapping("/chatroom/{chatroomidx}")
--- src/main/java/kr/omen/pico/service/CategoryService.java
+++ src/main/java/kr/omen/pico/service/CategoryService.java
@@ -1,19 +1,20 @@
 package kr.omen.pico.service;
 
-import kr.omen.pico.dao.CategoryRepository;
-import kr.omen.pico.domain.Category;
-import kr.omen.pico.domain.dto.CategoryDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import kr.omen.pico.dao.CategoryRepository;
+import kr.omen.pico.domain.Category;
+import kr.omen.pico.domain.dto.CategoryDTO;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
     public List<CategoryDTO> getAllCategory(){
         List<Category> list = categoryRepository.findAll();
